d74ff0fbdfd072396d990a829d6178fc73680bda
Thierry Laurion <thierry.laurion@gmail.com>
2018-01-30 20:29:19 -0500

    KGPE-D16 coreboot's SMB kvm support added for openbmc from https://review.coreboot.org/#/c/coreboot/+/19822/

---
+++ initrd/bin/flashrom-kgpe-d16.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+. /etc/functions
+
+ROM="$1"
+if [ -z "$1" ]; then
+	die "Usage: $0 /media/kgpe-d16.rom"
+fi
+
+cp "$ROM" /tmp/kgpe-d16.rom
+sha256sum /tmp/kgpe-d16.rom
+
+flashrom \
+	--force \
+	--noverify \
+	--programmer internal \
+	--image BIOS \
+	-w /tmp/kgpe-d16.rom \
+|| die "$ROM: Flash failed"
+
+warn "Reboot and hopefully it works..."
+exit 0
