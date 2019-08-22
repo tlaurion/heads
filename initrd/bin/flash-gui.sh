1951c6133674b1715a349eec911347a17c560bac
michael <github@riseup.net>
2019-08-22 18:52:59 +0000

    language clean-up

--- initrd/bin/flash-gui.sh
+++ initrd/bin/flash-gui.sh
@@ -7,7 +7,7 @@ set -e -o pipefail
 while true; do
   unset menu_choice
   whiptail --clear --title "Firmware Management Menu" \
-    --menu "Select the firmware function to perform\n\nRetaining settings copies existing settings to the new firmware:\n* Keeps your GPG keyring\n* Keeps Heads configuration changes\n\nErasing settings uses the new firmware as-is:\n* Erases any existing GPG keyring\n* Restores firmware to default factory settings\n\nIf you are just updating your firmware, you probably want to retain\nyour settings." 20 90 10 \
+    --menu "Select the firmware function to perform:\n\nRetain existing settings and copy to the new firmware:\n* Keeps your GPG keyring\n* Keeps Heads configuration changes\n\nErase existing settings and use the new firmware as-is:\n* Erases any existing GPG keyring\n* Restores firmware to default factory settings\n\nIf you are just updating your firmware, you probably want to retain\nyour settings." 20 90 10 \
     'f' ' Flash the firmware with a new ROM, retain settings' \
     'c' ' Flash the firmware with a new ROM, erase settings' \
     'x' ' Exit' \
@@ -21,7 +21,7 @@ while true; do
     ;;
     f|c )
       if (whiptail --title 'Flash the BIOS with a new ROM' \
-          --yesno "This requires you insert a USB drive containing:\n* Your BIOS image (*.rom)\n\nAfter you select this file, this program will reflash your BIOS\n\nDo you want to proceed?" 16 90) then
+          --yesno "This requires you insert a USB drive containing:\n* Your BIOS image (*.rom)\n\nAfter you select this file, the program will reflash your BIOS\n\nDo you want to proceed?" 16 90) then
         mount-usb || die "Unable to mount USB device."
         if grep -q /media /proc/mounts ; then
           find /media ! -path '*/\.*' -type f -name '*.rom' | sort > /tmp/filelist.txt
