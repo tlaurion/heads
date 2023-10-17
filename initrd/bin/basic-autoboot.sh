8e055119127f8740b51f0d5509b02ef1ca680b9c
Thierry Laurion <insurgo@riseup.net>
2023-10-17 09:38:40 -0400

    WiP: have detached signatures of hashes verified early when TOTP unseal fails

--- initrd/bin/basic-autoboot.sh
+++ initrd/bin/basic-autoboot.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 BOOT_MENU_OPTIONS=/tmp/basic-autoboot-options
 
-scan_boot_options /boot "grub.cfg" "$BOOT_MENU_OPTIONS"
+DO_WITH_DEBUG scan_boot_options /boot "grub.cfg" "$BOOT_MENU_OPTIONS"
 if [ -s "$BOOT_MENU_OPTIONS" ]; then
 	kexec-boot -b /boot -e "$(head -1 "$BOOT_MENU_OPTIONS")"
 fi
