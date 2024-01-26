18243083547fc33a0cf51a74bd8e591615e94593
Thierry Laurion <insurgo@riseup.net>
2024-01-25 21:43:10 -0500

    flake.nix: a lot of packages not needed when coreboot-toolstacks used.
    TODO: review
    
    Signed-off-by: Thierry Laurion <insurgo@riseup.net>

--- flake.nix
+++ flake.nix
@@ -10,58 +10,54 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = {
-    self,
-    flake-utils,
-    nixpkgs,
-    ...
-  }:
+  outputs = { self, flake-utils, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       devShell = pkgs.mkShellNoCC {
-        buildInputs = with pkgs;
-          [
-            autoconf
-            automake
-            bc
-            bison # Generate flashmap descriptor parser
-            cacert
-            cmake
-            cpio
-            curl
-            dtc
-            elfutils
-            flex
-            git
-            gnat11
-            innoextract
-            m4
-            ncurses5 # make menuconfig
-            perl
-            pkgconfig
-            python3
-            rsync
-            sharutils
-            texinfo
-            unzip
-            zip
-            wget
-            which
-            zlib.dev
-            imagemagick
-            vim
-            ccache
-          ]
-          ++ [
-            # qemu-coreboot-fbwhiptail-tpm2-hotp
-            libtool
-          ]
-          ++ [
-            # t440p
-            e2fsprogs
-            parted
-          ];
+        buildInputs = with pkgs; [
+          autoconf
+          automake
+          #bc
+          #bison # Generate flashmap descriptor parser
+          cacert
+          #canokey-qemu # qemu library support. Requires rebuilding qemu below.
+          cmake
+          cpio
+          curl
+          #dtc #device tree compiler: not sure needed.
+          #elfutils
+          #elfutils.dev #otherwise coreboot buidstack fails on nix deployed on top of quebesos 4.2 debian-12
+          flex
+          git
+          #gnat11
+          coreboot-toolchain.i386
+          coreboot-toolchain.ppc64
+          innoextract
+          m4
+          ncurses # make menuconfig
+          perl
+          pkgconfig
+          python3
+          #qemu #needed to test qemu-coreboot-* board configs
+          rsync
+          shadow #needed by tpm2-tss for groupadd/useradd otherwise fails to build
+          #sharutils
+          swtpm #needed to test qemu-coreboot-* board configs with qemu
+          texinfo
+          unzip
+          zip
+          wget
+          which
+          zlib.dev
+          imagemagick
+          vim
+          ccache
+          #(qemu.override {
+          #  canokeySupport = true; #Needed if qemu testing desired with virtual USB Security Dongle (config not yet added)
+          #  hostCpuOnly = true;
+          #})
+        ];
       };
     });
 }
