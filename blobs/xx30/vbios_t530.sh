2e5cbdf42b779a529671408bd8ced0995e41fa9a
Thierry Laurion <insurgo@riseup.net>
2024-05-10 13:30:53 -0400

    WiP: vbios scripts, try to adapt to nix
    Signed-off-by: Thierry Laurion <insurgo@riseup.net>

--- blobs/xx30/vbios_t530.sh
+++ blobs/xx30/vbios_t530.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e -o pipefail
+
 BLOBDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROMPARSER="94a615302f89b94e70446270197e0f5138d678f3"
 UEFIEXTRACT="UEFIExtract_NE_A58_linux_x86_64.zip"
@@ -12,66 +14,79 @@ BIOS_UPDATE_SHA256SUM="6fc652b5fa10e5ea1ddc0e8477a2f1cfe56e00df76a94629f9b736faa
 DGPU_ROM_SHA256SUM="3efe4886530086c0b612d1e669fbb4459ec93ec949b25a909e7ad273f4f01015  vbios_10de_0def_1.rom"
 IGPU_ROM_SHA256SUM="10b292c19322e7bb7db53350d2775d37b72a784292ea5686cd0f92af929f4916  vbios_8086_0106_1.rom"
 
+
 echo "### Creating temp dir"
 extractdir=$(mktemp -d)
 cd "$extractdir"
+echo "local dir: $(pwd)"
 
-echo "### Installing basic dependencies"
-sudo apt update && sudo apt install -y wget ruby ruby-dev bundler ruby-bundler p7zip-full upx-ucl 
-sudo gem install bundler:1.17.3
+#echo "### Installing basic dependencies"
+#sudo apt update && sudo apt install -y wget ruby ruby-dev bundler ruby-bundler p7zip-full upx-ucl 
+#Add local path to call executables within this script
+PATH="$PATH:$HOME/.nix-profile/bin"
 
 echo "### Downloading rom-parser dependency"
-wget https://github.com/awilliam/rom-parser/archive/"$ROMPARSER".zip || { echo "Failed to download $ROMPARSER" && exit 1; }
+wget -q https://github.com/awilliam/rom-parser/archive/"$ROMPARSER".zip || { echo "Failed to download $ROMPARSER" && exit 1; }
 
 echo "### Verifying expected hash of rom-parser"
 echo "$ROM_PARSER_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification..." && exit 1; }
 
 echo "### Installing rom-parser dependency"
 unzip "$ROMPARSER".zip
-cd rom-parser-"$ROMPARSER" && make
-sudo cp rom-parser /usr/sbin/
+cd rom-parser-"$ROMPARSER" && make > /dev/null 2>&1 
+#Add local path to call executables within this script
+PATH+="./"
+
 
 echo "### Downloading UEFIExtract dependency"
-wget https://github.com/LongSoft/UEFITool/releases/download/A58/"$UEFIEXTRACT" || { echo "Failed to download $UEFIEXTRACT" && exit 1; }
+wget -q https://github.com/LongSoft/UEFITool/releases/download/A58/"$UEFIEXTRACT" || { echo "Failed to download $UEFIEXTRACT" && exit 1; }
 
 echo "### Verifying expected hash of UEFIExtract"
 echo "$UEFI_EXTRACT_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification..." && exit 1; }
 
 echo "### Installing UEFIExtract"
-unzip "$UEFIEXTRACT"
-sudo mv UEFIExtract /usr/sbin/
+unzip "$UEFIEXTRACT"  > /dev/null 2>&1
+#Add local path to call executables within this script
+PATH+="./"
+echo "local dir: $(pwd)"
 
 echo "### Downloading VBiosFinder"
-wget https://github.com/coderobe/VBiosFinder/archive/"$VBIOSFINDER".zip || { echo "Failed to download $VBIOSFINDER" && exit 1; }
+wget -q https://github.com/coderobe/VBiosFinder/archive/"$VBIOSFINDER".zip || { echo "Failed to download $VBIOSFINDER" && exit 1; }
 
 echo "### Verifying expected hash of VBiosFinder"
 echo "$VBIOS_FINDER_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification..." && exit 1; }
 
 echo "### Installing VBiosFinder"
-unzip "$VBIOSFINDER".zip
-cd VBiosFinder-"$VBIOSFINDER" && bundle install --path=vendor/bundle
+unzip "$VBIOSFINDER".zip  > /dev/null 2>&1
+cd VBiosFinder-"$VBIOSFINDER" 
+bundle install
+#Add local path to call executables within this script
+PATH+="./"
+echo "local dir: $(pwd)"
+
 
 echo "### Downloading latest Lenovo bios update for t530"
-wget https://download.lenovo.com/pccbbs/mobiles/"$BIOSUPDATE" || { echo "Failed to download $BIOSUPDATE" && exit 1; }
+wget -q https://download.lenovo.com/pccbbs/mobiles/"$BIOSUPDATE" || { echo "Failed to download $BIOSUPDATE" && exit 1; }
 
 echo "### Verifying expected hash of bios update"
 echo "$BIOS_UPDATE_SHA256SUM" | sha256sum --check || { echo "Failed sha256sum verification..." && exit 1; }
 
 echo "### Extracting bios update"
-innoextract "$extractdir"/rom-parser-"$ROMPARSER"/VBiosFinder-"$VBIOSFINDER"/"$BIOSUPDATE" || { echo "Failed to extract $BIOSUPDATE" && exit 1; }
+innoextract "$extractdir"/rom-parser-"$ROMPARSER"/VBiosFinder-"$VBIOSFINDER"/"$BIOSUPDATE"  > /dev/null 2>&1 || { echo "Failed to extract $BIOSUPDATE" && exit 1; }
 
 echo "### Finding, extracting and saving vbios"
-sudo ./vbiosfinder extract "$extractdir"/rom-parser-"$ROMPARSER"/VBiosFinder-"$VBIOSFINDER"/"app/G4ETB7WW/\$01D5100.FL1" || { echo "Failed to extract FL1" && exit 1; }
+eval ./vbiosfinder extract "$extractdir"/rom-parser-"$ROMPARSER"/VBiosFinder-"$VBIOSFINDER"/"app/G4ETB7WW/\$01D5100.FL1" || { echo "Failed to extract FL1" && exit 1; }
 
 echo "Verifying expected hash of extracted roms"
 cd output
 echo "$DGPU_ROM_SHA256SUM" | sha256sum --check || { echo "dGPU rom failed sha256sum verification..." && exit 1; }
 echo "$IGPU_ROM_SHA256SUM" | sha256sum --check || { echo "iGPU rom Failed sha256sum verification..." && exit 1; }
+echo "local dir: $(pwd)"
 
 echo "### Moving extracted roms to blobs directory"
-sudo mv vbios_10de_0def_1.rom $BLOBDIR/10de,0def.rom
-sudo mv vbios_8086_0106_1.rom $BLOBDIR/8086,0106.rom
+mv vbios_10de_0def_1.rom $BLOBDIR/10de,0def.rom
+mv vbios_8086_0106_1.rom $BLOBDIR/8086,0106.rom
 
 echo "### Cleaning Up"
 cd "$BLOBDIR"
-sudo rm -rf "$extractdir"
+rm -rf "$extractdir"
